59a28616a62504426bb34b1f69058457b1a0c895
Marsel-777 <162730611+Marsel-777@users.noreply.github.com>
2024-03-16 10:18:54 +0300

    Update ShowPopup.tsx

--- src/components/ShowPopup.tsx
+++ src/components/ShowPopup.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 import {IoCloseCircleOutline} from 'react-icons/io5'
 
 type PropsType = {
-	addedCount: number
 	onShow: (arg0: object) => void
-	onClickAdd: () => void
 	pizza: {imageUrl: string, title: string, info: string, price: number}
 }
 
-const ShowPopup = ({pizza, onShow, onClickAdd, addedCount}: PropsType) => {
+const ShowPopup = ({pizza, onShow}: PropsType) => {
 	
 	return (
 		<div className='fullItem'>
@@ -17,15 +15,6 @@ const ShowPopup = ({pizza, onShow, onClickAdd, addedCount}: PropsType) => {
 				<img src={pizza.imageUrl}  alt='pizza'/>
 				<h2>{pizza.title}</h2>
 				<p>{pizza.info}</p>
-				<br/>
-				<h3>{pizza.price} ₽</h3>
-				<br/>
-					<button className="button button--outline button--add"
-					        onClick={onClickAdd}>
-						<span>В корзину</span>
-						{addedCount > 0 && <i>{addedCount}</i>}
-					</button>
-			</div>
 		</div>
 	)
 }
